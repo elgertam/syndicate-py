518a0c768345b3c887609ec32cf4aa0b36f9f4d6
Tony Garnock-Jones <tonyg@leastfixedpoint.com>
2021-11-30 18:24:11 +0100

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name="syndicate-py",
-    version="0.1.0",
+    version="0.2.0",
     author="Tony Garnock-Jones",
     author_email="tonyg@leastfixedpoint.com",
     license="GNU General Public License v3 or later (GPLv3+)",
