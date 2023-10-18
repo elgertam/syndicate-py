8bcfed2d4a06c2266c7b94fc232480ea9036811f
Tony Garnock-Jones <tonyg@leastfixedpoint.com>
2023-10-18 14:06:01 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name="syndicate-py",
-    version="0.13.0",
+    version="0.14.0",
     author="Tony Garnock-Jones",
     author_email="tonyg@leastfixedpoint.com",
     license="GNU General Public License v3 or later (GPLv3+)",
